a67ebd323bb293222fefc50419147bd0c805c8a2
Scott Barr <scottbarr@gmail.com>
2012-12-18 22:16:50 +0000

    Renamed tracker and removed unnecessary file

---
+++ tracker.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from api.lib.Stops import Stops
+from datetime import datetime
+
+app = Flask(__name__)
+
+STOPS = Stops()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', **get_page_data())
+
+
+@app.route('/track/<bus_stop_code>/')
+def search_by_stop_code(bus_stop_code):
+    return render_template('index.html', **get_page_data(bus_stop_code))
+
+
+def get_page_data(stop_code=36239637):
+    buses = STOPS.by_stop_code(stop_code)
+    return {
+        'year': datetime.now().year,
+        'buses': buses,
+        'span_width': int(12 / len(buses))
+    }
+
+if __name__ == '__main__':
+    app.run(debug=True)
